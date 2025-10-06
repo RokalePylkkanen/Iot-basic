0bdf80f2629ee97077ae1cf501f2ffdd67091ba5
Risto Piiroinen <risto.piiroinen@student.lab.fi>
2025-10-06 19:47:03 +0300

    maanantai ilta

---
+++ pico/KnightRiderTheOne.py
@@ -0,0 +1,67 @@
+from machine import Pin, PWM
+from utime import sleep
+
+buzzer = PWM(Pin(27))  
+
+# Kromaattinen asteikko C4 → C6 (Hz)
+notes = [
+    261, 277, 293, 311, 329, 349, 369, 392,
+    415, 440, 466, 493, 523, 554, 587, 622,
+    659, 698, 740, 784, 830, 880, 932, 988,
+    1047
+]
+
+# Melodian nuottien indeksit järjestyksessä notes listasta
+melodyindex = [
+    12, 13, 12, 12, 13, 12, 12, 13, 12,
+    12, 12, 11, 12, 12, 12
+]
+
+duration = 0.12  # Kuudestoistaosanuotti about...
+
+try:
+    while True:       
+        count1 = 0
+        count2 = 0
+        while count1 < 2:
+            for note in melodyindex:
+                if count2 < 1:
+                    buzzer.freq(notes[note])
+                    buzzer.duty_u16(32768)  # 50% teho
+                    sleep(0.12)
+                    buzzer.duty_u16(0)
+                    sleep(0.12) # kuudestoistaosa tauko
+                    count2 += 1
+                    continue
+                buzzer.freq(notes[note])
+                buzzer.duty_u16(32768)
+                sleep(duration )
+                buzzer.duty_u16(0)
+                sleep(0.01)
+            count1 += 1
+            count2 = 0
+           
+        count1 = 0
+        count2 = 0
+        while count1 < 2:
+            for note in melodyindex:
+                if count2 < 1:
+                    buzzer.freq(notes[note - 2])
+                    buzzer.duty_u16(32768)
+                    sleep(0.12)
+                    buzzer.duty_u16(0)
+                    sleep(0.12)
+                    count2 += 1
+                    continue
+                buzzer.freq(notes[note - 2])
+                buzzer.duty_u16(32768)
+                sleep(duration )
+                buzzer.duty_u16(0)
+                sleep(0.01)
+            count1 += 1
+            count2 = 0
+
+except KeyboardInterrupt:
+    buzzer.duty_u16(0)
+    buzzer.deinit()
+
---
+++ pico/sireeni1.py
@@ -0,0 +1,23 @@
+from machine import Pin, PWM
+from utime import sleep_ms
+
+buzzerPin = Pin(27)
+buzzer = PWM(buzzerPin)
+
+# Asetetaan aloitusarvot
+min_freq = 400   # Hz
+max_freq = 2000  # Hz
+step = 1        # Hz:n muutos per askel
+delay = 2       # ms viive jokaisen askeleen välillä
+
+buzzer.duty_u16(1000)  # Buzzer päälle (pieni arvo riittää)
+
+while True:
+    # Taajuus nousee
+    for f in range(min_freq, max_freq, step):
+        buzzer.freq(f)
+        sleep_ms(delay)
+    # Taajuus laskee
+    for f in range(max_freq, min_freq, -step):
+        buzzer.freq(f)
+        sleep_ms(delay)
